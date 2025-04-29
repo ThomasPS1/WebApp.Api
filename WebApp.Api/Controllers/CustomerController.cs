b77fe5047def9d4a524a8587e099fb40e11d26c8
Thomas <thomasps2002@gmail.com>
2025-04-29 11:41:45 +0530

    Add project files.

---
+++ WebApp.Api/Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Entity.Data;
+using WebApp.Entity.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController(ApplicationDbContext context) : ControllerBase
+    {
+        // GET: api/<EmployeeController>
+        [HttpGet]
+        public async Task<IEnumerable<Customer>> Get()
+        {
+            return await context.Customer.ToListAsync();
+        }
+
+        // GET api/<EmployeeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EmployeeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EmployeeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EmployeeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApp.Entity/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Entity.Models
+{
+    [Table("Customers")]
+    public class Customer
+    {
+        [Key]
+        public int customerId { get; set; }
+        public string? customName { get; set; }
+        public string? email { get; set; }
+        public long? contactNO { get; set; }
+        public string? location { get; set; }
+
+        //public List<Order>? Orders { get; set; }
+
+    }
+}
